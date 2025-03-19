136275975a6561b1915b9ce023c6834c923e0159
naumix <nauman.mic@gmail.com>
2025-03-19 10:45:58 -0700

    add

---
+++ with_compile/tdmpc2/scripts/5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# 
+ 
+source ~/miniconda3/bin/activate 
+conda init bash 
+source ~/.bashrc 
+conda activate tdmpc2 
+ 
+module load cuDNN/8.9.2.26-CUDA-12.2.0 
+python3 train.py task=humanoid9 seed=5
